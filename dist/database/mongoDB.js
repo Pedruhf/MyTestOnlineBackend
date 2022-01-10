56660c4de1929ac23b77b1e38ff5b9128da18397
Pedro Freitas <pedro.freitas.123@ufrn.edu.br>
2022-01-10 18:06:29 -0300

    atualiza chamada mongo na dist

--- dist/database/mongoDB.js
+++ dist/database/mongoDB.js
@@ -4,6 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const DB = "mongodb://localhost/MyTestOnline";
+const DB = process.env.MONGO_URL;
 mongoose_1.default.connect(DB);
 mongoose_1.default.connection.on("connected", () => { console.log("DB CONNECTED"); });
