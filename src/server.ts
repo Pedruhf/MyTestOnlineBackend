fcdd9767d785109edd976c175ba2983aaafbad37
Pedro Freitas <pedro.freitas.123@ufrn.edu.br>
2022-01-10 16:59:08 -0300

    Configura porta do server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
 require("dotenv").config();
 import { app } from './app';
 
-app.listen(3333, () => {
-  console.log("SERVER IS RUNNING AT THE DOOR 3333...")
-});
+app.listen(process.env.PORT || 3333);
